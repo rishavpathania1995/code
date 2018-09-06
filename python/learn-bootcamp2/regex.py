9c884e07f9ff1798d3bd620feeb7979d9058c802
Rishav <rishavpathania1995@gmail.com>
2018-09-06 20:27:56 +0530

    regex

---
+++ python/learn-bootcamp2/regex.py
@@ -0,0 +1,21 @@
+#https://pythex.org/
+#https://www.rexegg.com/regex-quickstart.html
+
+#\d  match digits0-9
+#\d\d match digit-0-9
+#\w match digit letter and under scores
+#\s whitespace character
+#\D not a digit
+#\W not a word
+#\S not a white space character
+# . any character except line space
+# +
+#{3}
+#{3,5}
+#{4,}
+# *
+# ? once
+#[aeiou]{2}
+#[^aeiou ] not aeiou
+# ^ must start with
+# $  end
